e3e70e23902b33974d3c210162f6ae4ff9c6a088
Pâmela Fidelis <pamella.fidelis@gmail.com>
2021-05-11 20:36:44 -0300

    refactor: include output path

--- model/model.py
+++ model/model.py
@@ -4,10 +4,14 @@ import os
 import boto3
 
 def predict_model(input_path, model_path):
+    output_path = os.path.abspath('data/output/score.csv')
+    
     ds = pd.read_csv(input_path)
     X = ds.iloc[:,:-1].values
 
     modelo = pickle.load(open(model_path, 'rb'))
     result = modelo.predict(X)
 
-    pd.DataFrame(result).to_csv(os.path.abspath('data/output/score.csv'))
+    pd.DataFrame(result).to_csv(output_path)
+
+    return output_path
