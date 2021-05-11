b0b0e0b101911d552f359bb29984c9948755f1a5
Pâmela Fidelis <pamella.fidelis@gmail.com>
2021-05-11 19:21:41 -0300

    refactor: remove s3 connection and trandformed into a method

--- model/model.py
+++ model/model.py
@@ -3,20 +3,11 @@ import pandas as pd
 import os
 import boto3
 
-BUCKET = "arq-batch-s3"
-dataset_path = os.path.abspath('../data/salary.csv')
-model_local_path = os.path.abspath('../model/predicting_salaries.pkl')
-score_path = os.path.abspath("../data/score.csv")
+def predict_model(input_path, model_path):
+    ds = pd.read_csv(input_path)
+    X = ds.iloc[:,:-1].values
 
-s3 = boto3.resource('s3')
-s3.Bucket(BUCKET).download_file('salary.csv', dataset_path)
-s3.Bucket(BUCKET).download_file('predicting_salaries.pkl', model_local_path)
+    modelo = pickle.load(open(model_path, 'rb'))
+    result = modelo.predict(X)
 
-ds = pd.read_csv('../data/salary.csv')
-X = ds.iloc[:,:-1].values
-
-modelo = pickle.load(open(model_local_path, 'rb'))
-result = modelo.predict(X)
-
-pd.DataFrame(result).to_csv(os.path.abspath(score_path))
-s3.Bucket(BUCKET).upload_file(score_path, "score.csv")
+    pd.DataFrame(result).to_csv(os.path.abspath('data/output/score.csv'))
