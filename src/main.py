a3d67d76aedb80aeb9ce89b45b44faccf6b68455
Pâmela Fidelis <pamella.fidelis@gmail.com>
2021-05-12 00:19:07 -0300

    refactor: main.py refactored

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-import boto3
-import sys
+import logging.config
 import yaml
-import logging
+import sys
 import os
 
 sys.path.append('.')
@@ -9,44 +8,64 @@ from utils.sqs import SQSQueue
 from utils.s3 import DataLake
 from model.model import predict_model
 
-# Load YAM
-def load_yaml():
-    with open('config/config.yaml') as file:
-        return yaml.safe_load(file)
 
-yaml_file = load_yaml()
+logging.basicConfig(filename='arq_batch.log',
+                format='%(levelname)s %(asctime)s :: %(message)s',
+                level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-# Load variables
-bucket_name = yaml_file['aws_components']['s3']['name']
 
-input_file_bucket = yaml_file['aws_components']['s3']['input_file']
-input_file_local = os.path.join('data/input', input_file_bucket.split('/')[-1])
+def load_config():
+    try:
+        with open('config/config.yaml') as file:
+            return yaml.safe_load(file)
+    except IOError as e:
+            logger.exception(f"I/O error({e.errno}): {e.strerror}")
+            raise e
+    except: 
+        logger.exception(f"Unexpected error: {sys.exc_info()[0]}")
 
-output_bucket = yaml_file['aws_components']['s3']['output_key']
 
-model_file_bucket = yaml_file['aws_components']['s3']['model_file']
+def execute_model(s3_bucket, file_input_bucket, model_path_bucket, file_output_bucket):
+    """
+    Download the model pickle and input files from S3, make the prediction and upload the output to S3
 
-queue_name = yaml_file['aws_components']['sqs']
+    :param s3_bucket: Bucket name
+    :param file_input_bucket: file input path from S3
+    :param model_path_bucket: pickle model path from S3
+    :param file_output_bucket: file output path into S3
 
-# call queue
-queue = SQSQueue(queue_name)
+    :return: None
+    """
 
-# mock a message
-queue.mock_send_message(bucket_name, input_file_bucket)
-s3_bucket, s3_bucket_key = queue.receive_message()
+    model_path_local = os.path.join('model', model_path_bucket.split('/')[-1])
+    input_path_local = os.path.join('data/input', file_input_bucket.split('/')[-1])
+    output_path_local = os.path.abspath('data/output/score.csv')
 
-s3 = DataLake(s3_bucket)
+    s3_client = DataLake(s3_bucket)
 
-# download of the model
-s3.download_file(model_file_bucket, model_file_bucket)
+    s3_client.download_file(model_path_bucket, model_path_local)
+    s3_client.download_file(file_input_bucket, input_path_local)
+    
+    predict_model(input_path_local, model_path_local, output_path_local)
 
-# download of file
-s3.download_file(input_file_bucket, input_file_local)
+    s3_client.upload_file(output_path_local, file_output_bucket)
 
-output_path = predict_model(input_file_local, model_file_bucket)
 
-output_bucket = os.path.join(output_bucket, output_path.split('/')[-1])
+def main(yaml_file):
+    file_output_bucket = yaml_file['aws_components']['s3']['output_key']
+    sqs_queue = yaml_file['aws_components']['sqs']
 
-s3.upload_file(output_path, output_bucket)
+    queue = SQSQueue(sqs_queue)
+    queue.mock_send_message()
 
-queue.delete_message()
+    s3_bucket, file_input_bucket, model_path_bucket = queue.receive_message()
+   
+    execute_model(s3_bucket, file_input_bucket, model_path_bucket, file_output_bucket)
+
+    queue.delete_message()
+
+if __name__ == "__main__":
+    yaml_file = load_config()
+
+    main(yaml_file)
