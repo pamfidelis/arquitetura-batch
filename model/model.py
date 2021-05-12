5819e147981efc9cbd02baf59fec7449d0c21867
Pâmela Fidelis <pamella.fidelis@gmail.com>
2021-05-12 00:16:57 -0300

    style: alter variable name

--- model/model.py
+++ model/model.py
@@ -1,11 +1,7 @@
 import pickle
 import pandas as pd
-import os
-import boto3
 
-def predict_model(input_path, model_path):
-    output_path = os.path.abspath('data/output/score.csv')
-    
+def predict_model(input_path, model_path, output_path):    
     ds = pd.read_csv(input_path)
     X = ds.iloc[:,:-1].values
 
@@ -13,5 +9,3 @@ def predict_model(input_path, model_path):
     result = modelo.predict(X)
 
     pd.DataFrame(result).to_csv(output_path)
-
-    return output_path
